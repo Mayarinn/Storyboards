0abaa291a8d0f9c773405fb36b7ae1c21aa02511
Karina Osadchaya <samara.black@mail.ru>
2024-06-03 12:53:14 +0300

    DEL: Old PushMe button

--- storyboard/ViewController.swift
+++ storyboard/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var StoryboardsImage: UIImageView!
     @IBOutlet weak var DescriptionLabel: UILabel!
     @IBOutlet weak var TextField: UITextField!
-    @IBOutlet weak var PushMeButton: UIButton!
     @IBOutlet weak var NewButton: UIButton!
     
     override func viewDidLoad() {
@@ -30,12 +29,7 @@ class ViewController: UIViewController {
         DescriptionLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         DescriptionLabel.numberOfLines = 0
         TextField.placeholder = "waiting for you"
-        PushMeButton.titleLabel?.text = "Push me"
         NewButton.setTitle("Push me", for: .normal)
-    }
-    
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        print("Hello")
     }
     
     @IBAction func newButtonTapped(_ sender: UIButton) {
